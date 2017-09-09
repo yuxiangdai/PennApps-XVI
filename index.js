e1966418337caa37f24fd00880be77ad1c416ff8
LWProgramming <lwgithub@gmail.com>
2017-09-08 21:12:52 -0400

    setup firebase properly with nodejs

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+var firebase = require("firebase");
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB-kYY6GjxJyLOo5a3WjwWy4Wton330Qqc",
+    authDomain: "pennapps-xvi-243c1.firebaseapp.com",
+    databaseURL: "https://pennapps-xvi-243c1.firebaseio.com",
+    projectId: "pennapps-xvi-243c1",
+    storageBucket: "",
+    messagingSenderId: "277333402514"
+    };
+    firebase.initializeApp(config);
+
 const express = require('express')
 const app = express()
 
